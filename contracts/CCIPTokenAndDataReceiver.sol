1ae56c5535e0fadd8cc474ed92c15524a47701c4
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-08-08 20:52:15 +0100

    [FEAT] Receiver request to mint nft and tokens

---
+++ contracts/CCIPTokenAndDataReceiver.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+
+contract MyNFT is ERC721URIStorage, OwnerIsCreator {
+    string constant TOKEN_URI =
+        "https://ipfs.io/ipfs/QmYuKY45Aq87LeL1R5dhb1hqHLp6ZFbJaCP8jxqKM1MX6y/babe_ruth_1.json";
+    uint256 internal tokenId;
+
+    constructor() ERC721("MyNFT", "MNFT") {}
+
+    function mint(address to) public onlyOwner {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, TOKEN_URI);
+        unchecked {
+            tokenId++;
+        }
+    }
+}
+
+contract CCIPTokenAndDataReceiver is CCIPReceiver, OwnerIsCreator {
+    MyNFT public nft;
+    uint256 price;
+    
+    event MintCallSuccessfull();
+    
+    constructor(address router, uint256 _price) CCIPReceiver(router) {
+        nft = new MyNFT();
+        price = _price;
+    }
+    
+    function _ccipReceive(
+        Client.Any2EVMMessage memory message
+    ) 
+        internal 
+        override 
+    {
+        require(message.destTokenAmounts[0].amount >= price, "Not enough CCIP-BnM for mint");
+        (bool success, ) = address(nft).call(message.data);
+        require(success);
+        emit MintCallSuccessfull();
+    }
+}
